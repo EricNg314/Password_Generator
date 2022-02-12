a5898e47489db6ea52bef5d2762941c68a469f45
Okens <Ng.Eric314@gmail.com>
2022-02-11 20:38:17 -0800

    Adding some validation and structure.

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ var letters = [
 var numeric = [
   "0","1","2","3","4","5","6","7","8","9"
 ];
-var nonNumeric = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","\\",":",";",'"',"'","<",",",">",".","?","/"
+var nonNumeric = [
+  "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","\\",":",";",'"',"'","<",",",">",".","?","/"
 ];
 
 
-var numberOfChar = 10; // TODO: update with prompt.
 
 // Write password to the #password input
 function writePassword() {
@@ -26,15 +26,44 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var pass = "";
-  
+  var numberOfChar = 10; // TODO: update with prompt.
+  var rerunAttempts = 0;
+
   var reqChar = {
     lower: true,
     upper: false,
     numeric: false,
-    nonNumeric: false,
+    nonNumeric: false
   };
 
-  
+  var containsChar = {
+    lower: false,
+    upper: false,
+    numeric: false,
+    nonNumeric: false
+  };
 
+  // TODO: Make function to assign characters.
+  // TODO: within function to assign, randomize between chosen sets.
+
+  var validation = validateRequest(reqChar, containsChar)
+
+  if (validation == false && rerunAttempts > 1){
+    console.log("Failed");
+    rerunAttempts++
+    generatePassword()
+  }
+  console.log("pass: ", pass)
   return pass;
+}
+
+function validateRequest (required, contains){
+  // Validates whether at least one character has been added per request.
+  var pwCriteria = true;
+  for (key in required){
+    if (required.key !== contains.key){
+      pwCriteria = false;
+    }
+  }
+  return pwCriteria;
 }
